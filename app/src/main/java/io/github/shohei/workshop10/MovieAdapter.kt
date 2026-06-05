a16e3258d7378caa6d205b0b8556767560064415
shohei-kasahara <shohei.kasahara0405@gmail.com>
2026-06-05 11:10:26 +1000

    Implement MovieAdapter and configure RecyclerView in MainActivity
    - Add `kotlin-parcelize` plugin and implement `Parcelable` in `MovieDataModel`.
    - Implement `MovieAdapter` skeleton and `MovieViewHolder`.
    - Initialize `RecyclerView` with `LinearLayoutManager` and `MovieAdapter` in `MainActivity`.
    - Rename `RecyclerView` ID to `recyclerviewMovies` in `activity_main.xml`.
    - Update `recycler_view_item.xml` with a background color for the poster image.
    - Define Kotlin and Parcelize plugins in `libs.versions.toml`.

--- app/src/main/java/io/github/shohei/workshop10/MovieAdapter.kt
+++ app/src/main/java/io/github/shohei/workshop10/MovieAdapter.kt
@@ -1,4 +1,42 @@
 package io.github.shohei.workshop10
 
-class MovieAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter(private val movieList: ArrayList<MovieDataModel>): RecyclerView.Adapter<MovieAdapter.MovieViewHolder>(){
+
+    class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        val moviePoster: ImageView = itemView.findViewById(R.id.ivPoster)
+        val movieName: TextView = itemView.findViewById(R.id.tvName)
+        val movieYear : TextView = itemView.findViewById(R.id.tvYear)
+
+
+    }
+    override fun onCreateViewHolder(
+        p0: ViewGroup,
+        p1: Int
+    ): MovieViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        p0: MovieViewHolder,
+        p1: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+
+
 }
